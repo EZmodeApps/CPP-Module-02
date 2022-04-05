7a4576e06450952817164ccd98b5ce6178422fad
EZmodeApps <75046274+EZmodeApps@users.noreply.github.com>
2022-04-05 18:34:19 +0300

    ex00 done

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -8,6 +8,22 @@ Fixed::~Fixed() {
 	std::cout << "Destructor called" << std::endl;
 }
 
+Fixed::Fixed(const Fixed &src) {
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+}
+
+Fixed &Fixed::operator = (const Fixed &src) {
+	std::cout << "Copy assigment operator called" << std::endl;
+	value = src.getRawBits();
+	return (*this);
+}
+
 int Fixed::getRawBits() const {
 	std::cout << "getRawBits member function called" << std::endl;
+	return (value);
+}
+
+void Fixed::setRawBits(int const raw) {
+	value = raw;
 }
--- ex00/Fixed.h
+++ ex00/Fixed.h
@@ -1,19 +1,21 @@
 #ifndef CPP_MODULE_02_FIXED_H
 #define CPP_MODULE_02_FIXED_H
 
+#include <iostream>
 
 class Fixed {
 public:
 	Fixed();
 	~Fixed();
-	Fixed(int cost n);
-	Fixed(Fixed const &number);
+	Fixed(int const n);
+	Fixed(Fixed const &src); //copy constructor
+	Fixed &operator= (const Fixed &src);
 	
 	int getRawBits(void) const;
 	void setRawBits(int const raw);
 
 private:
-	int number;
+	int value;
 	static const int bits = 8;
 };
 
