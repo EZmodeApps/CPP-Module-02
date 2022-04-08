3433d336b76f6a4855986a0fbc72a95083adb7bf
EZmodeApps <75046274+EZmodeApps@users.noreply.github.com>
2022-04-08 20:15:10 +0300

    ex01 done

--- ex01/Fixed.h
+++ ex01/Fixed.h
@@ -2,24 +2,29 @@
 #define CPP_MODULE_02_FIXED_H
 
 #include <iostream>
+#include <cmath>
 
 class Fixed {
 public:
 	Fixed();
-	~Fixed();
-	Fixed(int const n);
-	Fixed(float const n);
+	Fixed(const int raw);
+	Fixed(const float raw);
 	Fixed(Fixed const &src); //copy constructor
-	Fixed &operator= (const Fixed &src);
+	~Fixed();
+
+	Fixed &operator=(const Fixed &src);
+
 	int getRawBits(void) const;
-	void setRawBits(int const raw);
-	float toFloat( void ) const;
-	int toInt( void ) const;
+	void setRawBits(const int raw);
+
+	float toFloat(void) const;
+	int toInt(void) const;
 
 private:
 	int value;
 	static const int bits = 8;
 };
 
+std::ostream &operator<<(std::ostream &out, Fixed const &f);
 
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,24 +1,19 @@
 #include "Fixed.h"
 
-//std::ostream &operator<<(std::ostream &out, const Fixed &f) { //out - cout
-//	out << f.toFloat();
-//	return (out);
-//}
-
 int main( void ) {
 	Fixed a;
-//	Fixed const b( 10 );
+	Fixed const b( 10 );
 	Fixed const c( 42.2f );
-//	Fixed const d( b );
+	Fixed const d( b );
 	a = Fixed(1234.4321f );
-//	std::cout << "a is " << a << std::endl;
-//	std::cout << "b is " << b << std::endl;
-//	std::cout << "c is " << c << std::endl;
-//	std::cout << "d is " << d << std::endl;
-//	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-//	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-//	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-//	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 	return 0;
 }
 
